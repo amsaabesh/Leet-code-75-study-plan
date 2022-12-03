13bf0683cebad4c9df49c5f175fdb2cc74033306
amsaabesh <mahiraabesh@gmail.com>
2022-12-03 14:51:46 +0600

    1480 running sum

---
+++ 1480 leet/Program.cs
@@ -0,0 +1,15 @@
+﻿//1480. Running Sum of 1d Array
+public class Solution
+{
+    public int[] RunningSum(int[] nums)
+    {
+        var res = 0;
+        var list = new List<int>();
+        foreach (var n in nums)
+        {
+            res += n;
+            list.Add(res);
+        }
+        return list.ToArray();
+    }
+}
